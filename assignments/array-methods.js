56c370f1c4f8921eb1afa91cc77643c1e5d9c907
josepaniagua <jpanjr@gmail.com>
2019-04-18 22:50:54 -0700

    initialized array-methods.js, file

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -1,4 +1,5 @@
-// A local community center is holding a fund raising 5k fun run and has invited 50 small businesses to make a small donation on their behalf for some much needed updates to their facilities.  Each business has assigned a representative to attend the event along with a small donation.
+// A local community center is holding a fund raising 5k fun run and has invited 50 small businesses to make a small donation on their behalf for some much needed
+// updates to their facilities.  Each business has assigned a representative to attend the event along with a small donation.
 
 // Scroll to the bottom of the list to use some advanced array methods to help the event director gather some information from the businesses.
 
@@ -56,6 +57,7 @@ const runners = [{"id":1,"first_name":"Charmain","last_name":"Seiler","email":"c
 // ==== Challenge 1: Use .forEach() ====
 // The event director needs both the first and last names of each runner for their running bibs.  Combine both the first and last names into a new array called fullName. 
 let fullName = [];
+fullName = runners.map(person => `${person.first_name} ${person.last_name}`);
 console.log(fullName);
 
 // ==== Challenge 2: Use .map() ====
